1eee869d526cc54d349f581b86bfab4e1ba207a8
pratikagr <pratikben.1000@gmail.com>
2022-07-19 11:01:59 +0530

    onboarding changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myrx/screens/onboarding_one.dart'
 
 void main() {
   runApp(const MyApp());
@@ -12,67 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MyRx',
-      home: Scaffold(
-          body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          // Padding(padding: EdgeInsets.only(top: 2)),
-          //header
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Padding(padding: EdgeInsets.only(left: 16, right: 16)),
-              Card(
-                color: Colors.black,
-              ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  textStyle: const TextStyle(fontSize: 20),
-                ),
-                onPressed: () {},
-                child: const Text('Skip'),
-              ),
-            ],
-          ),
-
-          //image
-        Container(
-          height: 300,
-          width: 400,
-          child: Text('hello'),
-        decoration: BoxDecoration(
-          color: Color.fromRGBO(255, 0, 0, 100)
-          ),
-),
-          //text
-          Container(
-              child: Column(
-            children: [
-              Text('Title',style: TextStyle(
-                fontSize: 24,
-              ),),
-              SizedBox(
-                height: 24,
-              ),
-              Text('Description'),
-            ],
-          )),
-
-          //Button
-          ElevatedButton(
-            child: Text('Next'),
-            style: ElevatedButton.styleFrom(
-              fixedSize: Size(400, 55),
-              primary: Colors.green,
-              textStyle: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  fontStyle: FontStyle.normal),
-            ),
-            onPressed: () {},
-          ),
-        ],
-      )),
-    );
-  }
+      home: onboarding_one(context)
+          
+    )}
 }
